fc697f51fcdad9e85c445caf2a3f816f0506f403
BilalShahab999 <bilal.shahab99@gmail.com>
2025-09-02 11:00:30 +0500

    Delete response_logger.py

--- response_logger.py
+++
@@ -1,6 +0,0 @@
-def log_response(response):
-    print("==== MIDDLEWARE AFTER RESPONSE ====")
-    print("Status Code:", response.status)
-    print("Headers:", dict(response.headers))
-    print("===================================")
-    return response
