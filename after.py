b8fefa1c3395264c87b68c2da25331f67b1c4f52
BilalShahab999 <bilal.shahab99@gmail.com>
2025-09-02 10:58:14 +0500

    Delete after.py

--- after.py
+++
@@ -1,9 +0,0 @@
-
-def interceptor_after_request(response):
-    """Acts after every request (like an interceptor)"""
-    print("==== AFTER REQUEST INTERCEPTOR ====")
-    print("Status:", response.status)
-
-   
-    response.headers["X-Interceptor"] = "Active"
-    return response
