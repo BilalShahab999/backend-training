e745f89399f2041ba0ceba1bc71cd8eeebd43663
BilalShahab999 <bilal.shahab99@gmail.com>
2025-09-02 11:00:12 +0500

    Delete request_logger.py

--- request_logger.py
+++
@@ -1,8 +0,0 @@
-from flask import request, jsonify
-
-def log_request():
-    print("==== MIDDLEWARE BEFORE REQUEST ====")
-    print("Method:", request.method)
-    print("Path:", request.path)
-    print("Headers:", dict(request.headers))
-    print("===================================")
